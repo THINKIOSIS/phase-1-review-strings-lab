99b9e03ba8f0b1223b708bd3a75d1d2285f7bda4
THINKIOSIS <iqra.ndaro@student.moringaschool.com>
2022-08-22 01:25:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Write your code in this file!
+const currentUser = 'Cutie';
+const welcomeMessage = 'Welcome to Flatbook, Cutie!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, CUTIE!';
+const shortGreeting = 'Welcome, CUTIE!';// Write your code in this file!
